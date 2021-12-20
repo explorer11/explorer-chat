3447945e280806b42e3bd420167ec039e5358685
jgaspard <4029620+explorer11@users.noreply.github.com>
2021-12-20 08:30:21 +0100

    upgrade dependencies

--- chat-common/src/main/java/module-info.java
+++ chat-common/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module explorer.chat {
     requires org.apache.commons.lang3;
 	requires org.apache.commons.io;
-    requires slf4j.api;
+    requires org.slf4j;
     exports org.explorer.chat.common;
 }
--- chat-server/src/main/java/module-info.java
+++ chat-server/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module explorer.chat.server{
 	requires org.apache.commons.lang3;
 	requires org.apache.commons.io;
-	requires slf4j.api;
+	requires org.slf4j;
     requires explorer.chat;
     requires explorer.chat.data;
     requires explorer.chat.save;
